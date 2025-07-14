135d338ebd8b37da2d78805e35c25541c0ff4937
martynakruczek <martyna.kruczek@iteo.com>
2025-07-14 11:45:58 +0200

    Add test js function
    Signed-off-by: martynakruczek <martyna.kruczek@iteo.com>

---
+++ script.js
@@ -0,0 +1,22 @@
+const projects =[
+    //TODO:
+    {
+        //TODO:
+        img: 'mdasndkjnakj',
+        title: 'Project 1',
+    }
+]
+
+
+const showProjects =() => {
+    //TODO:
+    const getProjectsElm = document.getElementById("projects-container");
+
+    getProjectsElm.style.background = "white";
+    getProjectsElm.style.height = "200px";
+
+    getProjectsElm.innerHTML = '<div>dawsdada</div>'
+
+}
+
+showProjects();
