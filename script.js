0f9396ff6373f6974b47790985a6445373c54b2b
Justa367 <biernot54321@gmail.com>
2025-07-15 11:24:36 +0200

    TODO's done

--- script.js
+++ script.js
@@ -28,6 +28,17 @@ const projects = [
     }
 
 ]
+
+// Input from forms
+
+const messageFormInput={
+  name:name,
+  email:email,
+  subject:subject,
+  message:message
+};
+
+
 // Show projects
 
 const showProjects = (projectsToDisplay) => {
@@ -58,12 +69,17 @@ const showProjects = (projectsToDisplay) => {
 
 showProjects(projects);
 
-
 //TODO: Change styles for clicked and not clicked buttons
 document.querySelectorAll('.filter-btn').forEach(button => {
     button.addEventListener('click', () => {
         const filterValue = button.getAttribute('data-filter');
         const projectToShow = [];
+
+        // first remove "active" class
+        document.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
+
+        // Add "active" to clicked button
+        button.classList.add('active');
 
         projects.forEach(project => {
             const newTechTags = project.tech.map(tag =>
@@ -89,13 +105,6 @@ document.querySelectorAll('.filter-btn').forEach(button => {
     });
 });
 
-
-
-
-
-
-
-
 //different solution
 // const showFilteredProjects = (filter) => {
 //     const getProjectsElm = document.getElementById("projects-container");
@@ -103,7 +112,7 @@ document.querySelectorAll('.filter-btn').forEach(button => {
 //
 //     projects.filter(project => {
 //
-//         if (project.tech.map((x) => x?.toLowerCase()).includes(filter)) {
+ //         if (project.tech.map((x) => x?.toLowerCase()).includes(filter)) {
 //             getProjectsElm.innerHTML += `
 //                   <div class="project-card fade-in">
 //                     <div class="project-image">
@@ -149,6 +158,95 @@ document.querySelectorAll('.filter-btn').forEach(button => {
 //
 //     });
 // };
+
+// Email Validation on blure
+const emailInput = document.getElementById("email");
+
+// Without regex (mostly)
+/*
+emailInput.addEventListener("blur", ()=>{
+
+  const withoutSpaces=emailInput.value.replace(/\s+/g, "");
+  const email=withoutSpaces.trim();
+
+  const atIndex = email.indexOf('@');
+  const dotIndex = email.lastIndexOf('.');
+
+  // Sth in front of @,  dott after @ not next to, sth after dot
+  const isValid=atIndex>0 && dotIndex>atIndex+1&&dotIndex<email.length -1;
+  
+  alert(isValid ? 'Valid Email Address' : 'Invalid Email Address');
+  console.log(isValid);
+})*/
+
+// As function with html eventhandler onchange
+function validateEmail(inputElement) {
+  const withoutSpaces = inputElement.value.replace(/\s+/g, "");
+  const email = withoutSpaces.trim();
+
+  const atIndex = email.indexOf('@');
+  const dotIndex = email.lastIndexOf('.');
+
+  const isValid = atIndex > 0 && dotIndex > atIndex + 1 && dotIndex < email.length - 1;
+
+  const messageElement=document.getElementById("emailMessage");
+  messageElement.textContent=isValid? "Correct email":"Valid email";
+  messageElement.className = "error-message " + (isValid ? "valid" : "invalid");
+
+  return isValid;
+}S
+
+
+// Using regex
+/*
+emailInput.addEventListener("blur", () => {
+
+  const email = emailInput.value.trim();
+
+  //^ -> beginning of an input, [^\s@] -> Everything but without sings in bracket, \. -> dot must be there $ -> end of an input
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      emailInput.classList.add("input-error");
+      alert("Insert correct email.");
+    } else {
+      emailInput.classList.remove("input-error");
+    }
+});*/
+
+
+// Displaying inserted values in forms 
+function readFormData(event){
+  event.preventDefault()
+  const nameInput=document.getElementById("name").value;
+  const emailInput=document.getElementById("email").value;
+  const subjectInput=document.getElementById("subject").value;
+  const messageInput=document.getElementById("message").value;
+  console.log({
+    name:nameInput,
+    email:emailInput,
+    subjcet:subjectInput,
+    message:messageInput
+  });
+}
+
+// Smooth scrolling to project or contact section using button tag
+
+// With onclick
+function scrollToSection(selector){
+  document.querySelector(selector).scrollIntoView();
+}
+
+// Without onclick
+/*
+document.getElementById("scrollToProjects").addEventListener("click", () => {
+  document.querySelector("#projects").scrollIntoView();
+});
+
+document.getElementById("scrollToContact").addEventListener("click", () => {
+  document.querySelector("#contact").scrollIntoView();
+});
+*/
 
 
 
---
+++ test.js
@@ -0,0 +1,2 @@
+const stringgg="sdasadsad sdasdaasd sdasd";
+console.log(stringgg.trim());
